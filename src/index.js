ed25c35c7a1d2b89a4492c52f321f2cf9e6a72a4
ILowriderI <126571170+ILowriderI@users.noreply.github.com>
2023-02-28 16:35:34 +0200

    add changes

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
+import { REPO_NAME } from './constans/repo';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename={`/${REPO_NAME}/`}>
     <Provider store={store}>
     <App />
     </Provider>
